edadbff39b8c2e8c63e6983fc8e5c3f891ac0a70
Wendy <alteredco@gmail.com>
2019-01-07 12:40:13 +0100

    add gulp as a dev dependency

---
+++ v1/gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require("gulp");
+
+gulp.task("default", () => {
+  console.log("Hooray for gulp!! It's working!!");
+});
+
+gulp.task("html", ()=> {
+  console.log("Imagine something useful being done to your HTML here!");
+});
--- v1/readme.md
+++ v1/readme.md
@@ -1,5 +1,10 @@
 #MommaCafe
 
+Dev dependencies:
+Please install GULP both globally and locally.
+  npm install gulp-cli --global
+  npm install gulp --save-dev 
+
 *Add landing page
 *Add Cafe page that lists all cafes
 
