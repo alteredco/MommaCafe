643933a07b3c873b6e4258e9b06dfc8c4a984bb3
Wendy <alteredco@gmail.com>
2019-01-07 17:01:17 +0100

    add gulp-sass converter from sass to css

--- v1/gulpfile.js
+++ v1/gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass");
 
 gulp.task("default", () => {
   console.log("Hooray for gulp!! It's working!!");
@@ -6,4 +7,11 @@ gulp.task("default", () => {
 
 gulp.task("html", ()=> {
   console.log("Imagine something useful being done to your HTML here!");
+});
+
+//converts sass to css with gulp-sass
+gulp.task("sass", function() {
+  return gulp.src("app/scss/styles.scss")
+  .pipe(sass()) 
+  .pipe(gulp.dest("app/css"))
 });
