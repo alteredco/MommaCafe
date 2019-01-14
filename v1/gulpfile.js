368c887d91cebd0b214ad1c0134dc4d5b8ad263b
Wendy <alteredco@gmail.com>
2019-01-14 12:32:06 +0100

    sync node and gulp to run nodemon and gulp browser-sync in harmony

--- v1/gulpfile.js
+++ v1/gulpfile.js
@@ -1,17 +1,65 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
+const gulp = require("gulp"),
+sass = require("gulp-sass"),
+browserSync = require("browser-sync").create(),
+nodemon = require("gulp-nodemon"),
+watch = require("gulp-watch");
+
 
 gulp.task("default", () => {
   console.log("Hooray for gulp!! It's working!!");
 });
 
-gulp.task("html", ()=> {
-  console.log("Imagine something useful being done to your HTML here!");
+gulp.task("styles", () => {
+  console.log("imagine SASS task happening here.");
+});
+
+
+// starts the gulp server
+gulp.task("nodemon", cb => {
+  let started = false;
+
+  return nodemon({
+    script: "./app/app.js",
+    ignore: [
+      'gulpfile.js',
+      'node_modules/'
+    ]
+  }).on("start", () => {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
+
+// syncs server with browser
+gulp.task(
+  "browser-sync",
+  gulp.series("nodemon", () => {
+    browserSync.init(null, {
+      proxy: "http://localhost:3000",
+      files: ["./app/*.js"],
+
+      port: 5000
+    });
+  })
+);
+
+gulp.task("serve", gulp.series("browser-sync", () => {}));
+
+// syncs reload of page with actions
+gulp.task("watch", () => {
+  watch("./app/*.js", () => {
+    gulp.start("default")
+  })
+  watch("./app/assets/css/**/*.css", () =>{
+    gulp.start("styles")
+  })
 });
 
 //converts sass to css with gulp-sass
-gulp.task("sass", function() {
-  return gulp.src("app/scss/styles.scss")
+gulp.task("sass", () => {
+  return gulp.src("./app/assets/scss/styles.scss")
   .pipe(sass()) 
-  .pipe(gulp.dest("app/css"))
+  .pipe(gulp.dest("./app/assets/css"))
 });
